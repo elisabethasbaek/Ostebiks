e6b6f792c3a7a529a6b98c924b4e8bf05800b9e6
elisabethasbaek <elisabeth.asbaek@gmail.com>
2020-12-18 10:53:26 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+var express = require("express");
+var app = express();
+var formidable = require("express-formidable");
+var cors = require("cors");
+
+require("./database");
+app.use(cors());
+app.use(formidable());
+
+require("./cheese.route")(app);
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log("App is running on port", process.env.PORT || 3000);
+});
